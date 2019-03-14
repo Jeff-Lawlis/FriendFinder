b0d13a24ca433af72747ffa6c15b170dda4d416e
Jeff Lawlis <jeff.lawlis@yahoo.com>
2019-03-14 15:40:39 -0500

    added data fields

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+var friends = [{
+    "name": "Michael Scott",
+    "imageURL": "https://img.buzzfeed.com/buzzfeed-static/static/2017-05/16/13/asset/buzzfeed-prod-fastlane-03/sub-buzz-29780-1494955460-2.jpg?downsize=700:*&output-format=auto&output-quality=auto",
+    "submissions": [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+},
+{
+    "name": "Jim Halpert",
+    "imageURL": "https://pbs.twimg.com/profile_images/3171824697/ef75d90df2e65ce326acf30262df5918_400x400.jpeg",
+    "submissions": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+},
+{
+    "name": "Dwight Schrute",
+    "imageURL": "https://brobible.files.wordpress.com/2018/10/best-of-dwight-schrute-the-office-compilation.jpg?quality=90&w=650",
+    "submissions":[3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+},
+{
+    "name": "Pam Beesly",
+    "imageURL": "https://s1.r29static.com//bin/entry/ef5/720x1080,85/1741136/image.webp",
+    "submissions": [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+},
+
+{
+    "name": "Angela Martin",
+    "imageURL": "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-the-office-angela-kinsey.jpg",
+    "submissions":[2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+},
+
+{
+    "name": "Kevin Malone",
+    "imageURL": "https://upload.wikimedia.org/wikipedia/en/thumb/6/60/Office-1200-baumgartner1.jpg/260px-Office-1200-baumgartner1.jpg",
+    "submissions": [2, 1, 2, 1, 2, 1, 2, 1, 2, 1]
+},
+
+{
+    "name": "Creed Bratton",
+    "imageURL": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Creed_Bratton.jpg/220px-Creed_Bratton.jpg",
+    "submissions": [5, 4, 5, 4, 5, 4, 5, 4, 5, 4]
+}
+]
